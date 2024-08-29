f1bcd3be344bdd4e807f34917d74228f7bf6f053
SantiagoA04567 <santago04524@gmail.com>
2024-08-29 16:48:55 -0500

    Proyecto greenfoot santiago amariles
    este es el proyecto echo en clase de greenfoot

---
+++ 29-08-2024 Santiago Amariles/Avion.java
@@ -0,0 +1,35 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Avion here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Avion extends Actor
+{
+    /**
+     * Act - do whatever the Avion wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public Avion(int cantidad){
+        setRotation(cantidad);
+    }
+    public void act()
+    {
+        if(Greenfoot.isKeyDown("right")){
+            setLocation(getX()+5, getY());
+        }
+        if(Greenfoot.isKeyDown("left")){
+            setLocation(getX()-5, getY());
+        }
+        if(Greenfoot.isKeyDown("up")){
+            setLocation(getX(), getY()-5);
+        }
+        if(Greenfoot.isKeyDown("down")){
+            setLocation(getX(), getY()+5);
+        }
+        
+        // Add your action code here.
+    }
+}
