badef6571dbd732aa08c56e5dd12c17706319dae
SantiagoA04567 <santago04524@gmail.com>
2024-09-23 19:14:12 -0500

    Proyecto de Hola Mundo en C#
    Este es el hola mundo en el lenguaje de programación de C#

---
+++ HolaMundo/Form1.cs
@@ -0,0 +1,15 @@
+namespace HolaMundo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MostrarMensaje_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola mundo");
+        }
+    }
+}
